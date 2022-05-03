04f6016d73da78a80579182cde0a701e10c4105c
rudilazuli <100395138+rudilazuli@users.noreply.github.com>
2022-05-03 06:11:45 -0700

    Create LensSlices.py
    Len's Slices in a project for understanding lists in Python.

---
+++ LensSlices.py
@@ -0,0 +1,34 @@
+# Your code below:
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+# step 2
+prices = [2, 6, 1, 3, 2, 7, 2]
+# step 3
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+# step 4
+num_pizzas = len(toppings)
+print(num_pizzas)
+# step 5
+num_pizzas = "We sell 7 different kinds of pizza!"
+print(num_pizzas)
+# step 6
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+# step 7
+print(pizza_and_prices)
+# step 8
+pizza_and_prices.sort()
+# step 9
+cheapest_pizza = pizza_and_prices[0]
+print(cheapest_pizza)
+[1, 'cheese']
+# step 10
+priciest_pizza = pizza_and_prices[-1]
+print(priciest_pizza)
+# step 11
+pizza_and_prices.pop()
+# step 12
+pizza_and_prices.insert(4, [2.5, "peppers"])
+print(pizza_and_prices)
+# step 13
+three_cheapest = pizza_and_prices[:3]
+print(three_cheapest)
